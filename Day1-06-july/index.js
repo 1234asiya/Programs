66cde8a9709b3697e7282dfb88d748de7e716581
1234asiya <mrcewasiya@gmail.com>
2023-07-07 12:20:02 +0530

    done post data through html

--- Day1-06-july/index.js
+++ Day1-06-july/index.js
@@ -1,12 +1,21 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const PORT = 9003
-app.set("view engine","ejs")
-app.get("/:route", (request, response) => {
-    const param1=request.params["route"]
-    response.render(`${param1}`)
+var bodyParser = require('body-parser');  
+var urlencodedParser = bodyParser.urlencoded({ extended: false })  
+const PORT = 9000
+
+app.get("/post.html",(request,response)=>{
+    response.sendFile(path.join(__dirname,"./post.html"))
 })
+app.post('/postpath', urlencodedParser, function (req, res) {    
+    response = {  
+        first_name:req.body.first_name,  
+        last_name:req.body.last_name  
+    };  
+    console.log(response);  
+    res.end(JSON.stringify(response));  
+ })  
 app.listen(PORT, () => { console.log("Server running at Port ", PORT) })
 
 
